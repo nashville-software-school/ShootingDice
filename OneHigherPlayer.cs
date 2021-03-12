f1d2ed79776502e53a8c2740739dde03bc7b0971
Andy Collins <askingalot@gmail.com>
2021-03-12 16:07:03 -0600

    Improve instructions for OneHigherPlayer

--- OneHigherPlayer.cs
+++ OneHigherPlayer.cs
@@ -2,7 +2,7 @@ namespace ShootingDice
 {
     // TODO: Complete this class
 
-    // A Player who always roles one higher than the other player
+    // Override the Play method to make a Player who always roles one higher than the other player
     public class OneHigherPlayer
     {
 
